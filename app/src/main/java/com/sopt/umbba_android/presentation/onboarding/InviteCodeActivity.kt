299af6c36a1f01a23972ce2d96492bbe8415aa2b
ss99x2002 <ss99x200211@gmail.com>
2023-07-03 22:56:01 +0900

    [Chore/#1] 코드정렬

--- app/src/main/java/com/sopt/umbba_android/presentation/onboarding/InviteCodeActivity.kt
+++ app/src/main/java/com/sopt/umbba_android/presentation/onboarding/InviteCodeActivity.kt
@@ -4,6 +4,7 @@ import com.sopt.umbba_android.R
 import com.sopt.umbba_android.databinding.ActivityInviteCodeBinding
 import com.sopt.umbba_android.util.binding.BindingActivity
 
-class InviteCodeActivity:BindingActivity<ActivityInviteCodeBinding>(R.layout.activity_invite_code){
+class InviteCodeActivity :
+    BindingActivity<ActivityInviteCodeBinding>(R.layout.activity_invite_code) {
 
 }
