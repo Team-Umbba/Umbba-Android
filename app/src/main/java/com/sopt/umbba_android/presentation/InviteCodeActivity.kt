25ae9fecbe1cc1e159b1f511929903c4f2c9f4bc
ss99x2002 <ss99x200211@gmail.com>
2023-07-03 21:31:26 +0900

    [Design/#1] 초대코드 입력 뷰 구현

---
+++ app/src/main/java/com/sopt/umbba_android/presentation/InviteCodeActivity.kt
@@ -0,0 +1,9 @@
+package com.sopt.umbba_android.presentation
+
+import com.sopt.umbba_android.R
+import com.sopt.umbba_android.databinding.ActivityInviteCodeBinding
+import com.sopt.umbba_android.util.binding.BindingActivity
+
+class InviteCodeActivity:BindingActivity<ActivityInviteCodeBinding>(R.layout.activity_invite_code){
+
+}
