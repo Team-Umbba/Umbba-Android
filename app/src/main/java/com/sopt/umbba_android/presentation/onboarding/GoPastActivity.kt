3a2f8b5fd60f953122032cca1e7b17d8e23ad87c
ss99x2002 <ss99x200211@gmail.com>
2023-07-04 17:32:31 +0900

    [Design/#4] 시간 통지 화면 구현

---
+++ app/src/main/java/com/sopt/umbba_android/presentation/onboarding/GoPastActivity.kt
@@ -0,0 +1,9 @@
+package com.sopt.umbba_android.presentation.onboarding
+
+import com.sopt.umbba_android.R
+import com.sopt.umbba_android.databinding.ActivityGoPastBinding
+import com.sopt.umbba_android.util.binding.BindingActivity
+
+class GoPastActivity : BindingActivity<ActivityGoPastBinding>(R.layout.activity_go_past) {
+
+}
