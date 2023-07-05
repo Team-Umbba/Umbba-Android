4eac9606714a96c090438c53c4dce3eb13ade853
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-07-05 17:19:22 +0900

    [Design/#7] InviteActivity UI 구현

---
+++ app/src/main/java/com/sopt/umbba_android/presentation/onboarding/InviteActivity.kt
@@ -0,0 +1,13 @@
+package com.sopt.umbba_android.presentation.onboarding
+
+import android.os.Bundle
+import com.sopt.umbba_android.R
+import com.sopt.umbba_android.databinding.ActivityInviteBinding
+import com.sopt.umbba_android.util.binding.BindingActivity
+
+class InviteActivity : BindingActivity<ActivityInviteBinding>(R.layout.activity_invite) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
