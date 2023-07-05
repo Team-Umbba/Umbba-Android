9eda03a681fa810cd1a96f338b7a06b08a7219b6
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-07-05 17:19:03 +0900

    [Design/#7] LoginActivity UI 구현

---
+++ app/src/main/java/com/sopt/umbba_android/presentation/onboarding/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.sopt.umbba_android.presentation.onboarding
+
+import android.os.Bundle
+import com.sopt.umbba_android.R
+import com.sopt.umbba_android.databinding.ActivityLoginBinding
+import com.sopt.umbba_android.util.binding.BindingActivity
+
+class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_login) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
