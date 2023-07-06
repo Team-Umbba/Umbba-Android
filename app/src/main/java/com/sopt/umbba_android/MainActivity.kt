87c788fdd14f20e1953882753ec90d019d99f5fd
ss99x2002 <ss99x200211@gmail.com>
2023-07-06 18:31:15 +0900

    [Design/#12] qna 커스텀 appbar 수정

--- app/src/main/java/com/sopt/umbba_android/MainActivity.kt
+++ app/src/main/java/com/sopt/umbba_android/MainActivity.kt
@@ -3,12 +3,13 @@ package com.sopt.umbba_android
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sopt.umbba_android.presentation.AnswerActivity
 import com.sopt.umbba_android.presentation.onboarding.NotifyTimeActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        startActivity(Intent(this, NotifyTimeActivity::class.java))
+        startActivity(Intent(this, AnswerActivity::class.java))
     }
 }
