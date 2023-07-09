6675bcb50cdade36e8000428a03accc5bde2f7d5
ss99x2002 <ss99x200211@gmail.com>
2023-07-10 01:24:58 +0900

    [Design/#26] BackAnswerDialog 구현

---
+++ app/src/main/java/com/sopt/umbba_android/presentation/qna/BackAnswerDialogFragment.kt
@@ -0,0 +1,41 @@
+package com.sopt.umbba_android.presentation.qna
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.fragment.app.DialogFragment
+import com.sopt.umbba_android.databinding.FragmentConfirmAnswerDialogBinding
+
+class BackAnswerDialogFragment : DialogFragment() {
+
+    private var _binding: FragmentConfirmAnswerDialogBinding? = null
+    private val binding get() = requireNotNull(_binding) { "BackAnswerDialogFragment is null" }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentConfirmAnswerDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        dialog?.window?.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
