053965df27ebcff41f9a9fdb8ccdcd01f0a66b7a
ss99x2002 <ss99x200211@gmail.com>
2023-07-09 13:15:28 +0900

    [Design/#14] 문답 화면 구현

---
+++ app/src/main/java/com/sopt/umbba_android/presentation/QuestionAnswerActivity.kt
@@ -0,0 +1,10 @@
+package com.sopt.umbba_android.presentation
+
+import com.sopt.umbba_android.R
+import com.sopt.umbba_android.databinding.ActivityQuestionAnswerBinding
+import com.sopt.umbba_android.util.binding.BindingActivity
+
+class QuestionAnswerActivity :
+    BindingActivity<ActivityQuestionAnswerBinding>(R.layout.activity_question_answer) {
+
+}
