7960e8e3592e7edf09cc97da569668ef9ce51c6d
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-07-10 00:10:45 +0900

    [Design/#20] ConfirmAnswerDialogFragment UI 구현

---
+++ app/src/main/java/com/sopt/umbba_android/presentation/qna/ConfirmAnswerDialogFragment.kt
@@ -0,0 +1,26 @@
+package com.sopt.umbba_android.presentation.qna
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.sopt.umbba_android.R
+import com.sopt.umbba_android.databinding.FragmentConfirmAnswerDialogBinding
+
+class ConfirmAnswerDialogFragment : DialogFragment() {
+
+    private var _binding: FragmentConfirmAnswerDialogBinding? = null
+    private val binding get() = requireNotNull(_binding) { "ConfirmAnswerDialogFragment is null" }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentConfirmAnswerDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+}
