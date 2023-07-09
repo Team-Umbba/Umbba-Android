4014b1ce046555f8a06ac6cddffa5729714227af
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-07-09 22:27:02 +0900

    [Design/#20] InviteCodeDialogFragment UI 구현

---
+++ app/src/main/java/com/sopt/umbba_android/presentation/home/InviteCodeDialogFragment.kt
@@ -0,0 +1,49 @@
+package com.sopt.umbba_android.presentation.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.fragment.app.DialogFragment
+import com.sopt.umbba_android.R
+import com.sopt.umbba_android.databinding.FragmentInviteCodeDialogBinding
+
+class InviteCodeDialogFragment : DialogFragment() {
+
+    private var _binding: FragmentInviteCodeDialogBinding? = null
+    private val binding get() = requireNotNull(_binding) { "InviteCodeDialogFragment is null" }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentInviteCodeDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        closeDialog()
+    }
+
+    private fun closeDialog() {
+        binding.btnClose.setOnClickListener {
+            dismiss()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        dialog?.window?.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
