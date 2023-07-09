e8a0ae557eeeedbccc5210322c0c7e565c51932f
ss99x2002 <ss99x200211@gmail.com>
2023-07-09 22:13:18 +0900

    [Feat/#19] 홈 뷰 배경설정

--- app/src/main/java/com/sopt/umbba_android/presentation/MainActivity.kt
+++ app/src/main/java/com/sopt/umbba_android/presentation/MainActivity.kt
@@ -3,13 +3,22 @@ package com.sopt.umbba_android.presentation
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.sopt.umbba_android.R
+import com.sopt.umbba_android.databinding.ActivityMainBinding
+import com.sopt.umbba_android.presentation.home.HomeFragment
 import com.sopt.umbba_android.presentation.qna.QuestionAnswerActivity
+import com.sopt.umbba_android.util.binding.BindingActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity :BindingActivity<ActivityMainBinding>(R.layout.activity_main){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        startActivity(Intent(this, QuestionAnswerActivity::class.java))
+        changeFragment(HomeFragment())
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_fv, fragment)
+            .commit()
     }
 }
