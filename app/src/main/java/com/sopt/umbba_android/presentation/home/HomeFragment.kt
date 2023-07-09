29cd049957748aec855670fe431852f202a1dc8a
ss99x2002 <ss99x200211@gmail.com>
2023-07-09 21:20:02 +0900

    [Design/#19] 홈 fragment binding

--- app/src/main/java/com/sopt/umbba_android/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/sopt/umbba_android/presentation/home/HomeFragment.kt
@@ -1,4 +1,19 @@
 package com.sopt.umbba_android.presentation.home
 
-class HomeFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sopt.umbba_android.R
+import com.sopt.umbba_android.databinding.FragmentHomeBinding
+import com.sopt.umbba_android.util.binding.BindingFragment
+
+class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 }
