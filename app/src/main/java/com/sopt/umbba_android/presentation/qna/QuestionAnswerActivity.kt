51f239857065c5e87a64f36d9cceb303242bdbcb
ss99x2002 <ss99x200211@gmail.com>
2023-07-10 22:49:48 +0900

    [Design/#35] custom appbar 클릭 리스너 적용

--- app/src/main/java/com/sopt/umbba_android/presentation/qna/QuestionAnswerActivity.kt
+++ app/src/main/java/com/sopt/umbba_android/presentation/qna/QuestionAnswerActivity.kt
@@ -9,15 +9,22 @@ import com.sopt.umbba_android.util.binding.BindingActivity
 
 
 class QuestionAnswerActivity :
-    BindingActivity<ActivityQuestionAnswerBinding>(R.layout.activity_question_answer) {
+    BindingActivity<ActivityQuestionAnswerBinding>(R.layout.activity_question_answer),View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.clickListener=this
         setBlurText(true)
         with(binding) {
             btnAnswer.setOnClickListener {
                 setBlurText(false) // 임시 블러 테스트
             }
         }
+    }
+
+    override fun onClick(view: View?) {
+       when(view?.id){
+           R.id.iv_qna_back->finish()
+       }
     }
 
     private fun setBlurText(isBlur: Boolean) {
