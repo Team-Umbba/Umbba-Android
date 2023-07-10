32d15b2729af0a21d32d2735b971e071d21a5c97
ss99x2002 <ss99x200211@gmail.com>
2023-07-10 21:51:36 +0900

    [Feat/#35] 설정 화면 클릭이벤트 설정

--- app/src/main/java/com/sopt/umbba_android/presentation/setting/ManageAccountActivity.kt
+++ app/src/main/java/com/sopt/umbba_android/presentation/setting/ManageAccountActivity.kt
@@ -1,9 +1,25 @@
 package com.sopt.umbba_android.presentation.setting
 
+import android.content.Intent
+import android.os.Bundle
 import com.sopt.umbba_android.R
 import com.sopt.umbba_android.databinding.ActivityManageAccountBinding
 import com.sopt.umbba_android.util.binding.BindingActivity
 
 class ManageAccountActivity: BindingActivity<ActivityManageAccountBinding>(R.layout.activity_manage_account) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setClickEvent()
+    }
 
+    private fun setClickEvent(){
+        with(binding){
+            clLogout.setOnClickListener {
+                TODO("서버 로그아웃 API 연결")
+            }
+            clDeleteAccount.setOnClickListener {
+                startActivity(Intent(this@ManageAccountActivity,DeleteAccountActivity::class.java))
+            }
+        }
+    }
 }
--- app/src/main/java/com/sopt/umbba_android/presentation/setting/SettingFragment.kt
+++ app/src/main/java/com/sopt/umbba_android/presentation/setting/SettingFragment.kt
@@ -1,5 +1,6 @@
 package com.sopt.umbba_android.presentation.setting
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import com.sopt.umbba_android.R
@@ -9,5 +10,23 @@ import com.sopt.umbba_android.util.binding.BindingFragment
 class SettingFragment:BindingFragment<FragmentSettingBinding>(R.layout.fragment_setting) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setClickEvent()
     }
+    private fun setClickEvent(){
+        with(binding){
+            clManageAccount.setOnClickListener {
+                startActivity(Intent(requireActivity(),ManageAccountActivity::class.java))
+            }
+            clAboutUmbba.setOnClickListener{
+                TODO("엄빠소개 노션으로 이동하도록 만들기. Uri 연결하기")
+            }
+            clTos.setOnClickListener {
+                TODO("이용약관 노션으로 이동하도록 만들기. Uri 연결하기")
+            }
+            clNotice.setOnClickListener {
+                TODO("개인정보처리방침 노션으로 이동하도록 만들기. Uri 연결하기")
+            }
+        }
+    }
+
 }
