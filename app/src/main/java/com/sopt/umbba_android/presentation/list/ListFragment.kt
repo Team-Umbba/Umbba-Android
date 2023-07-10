dc05563e732bc02e2b9ff61f2ba7d2611d913924
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-07-10 12:50:13 +0900

    [Design/#25] ListFragment UI 구현

---
+++ app/src/main/java/com/sopt/umbba_android/presentation/list/ListFragment.kt
@@ -0,0 +1,19 @@
+package com.sopt.umbba_android.presentation.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sopt.umbba_android.R
+import com.sopt.umbba_android.databinding.FragmentListBinding
+import com.sopt.umbba_android.util.binding.BindingFragment
+
+class ListFragment : BindingFragment<FragmentListBinding>(R.layout.fragment_list) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
