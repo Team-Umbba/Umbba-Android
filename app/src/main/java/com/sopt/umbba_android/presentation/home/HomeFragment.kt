5bb8d8237e384da3ebf5bc4679c2b4573824504f
ss99x2002 <ss99x200211@gmail.com>
2023-07-10 22:58:39 +0900

    [Design/#35] 질문 답변하기 버튼 연결

--- app/src/main/java/com/sopt/umbba_android/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/sopt/umbba_android/presentation/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.sopt.umbba_android.presentation.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,14 +8,23 @@ import android.view.ViewGroup
 import coil.load
 import com.sopt.umbba_android.R
 import com.sopt.umbba_android.databinding.FragmentHomeBinding
+import com.sopt.umbba_android.presentation.qna.QuestionAnswerActivity
 import com.sopt.umbba_android.util.binding.BindingFragment
 
 class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setBackground()
+        setClickEvent()
     }
 
+    private fun setClickEvent(){
+        with(binding){
+            btnAnswer.setOnClickListener {
+                startActivity(Intent(requireActivity(), QuestionAnswerActivity::class.java))
+            }
+        }
+    }
     private fun setBackground(){
         with(binding){
             ivBackground.load("https://i.ibb.co/sRV9Vr4/iv-maru.jpg")
