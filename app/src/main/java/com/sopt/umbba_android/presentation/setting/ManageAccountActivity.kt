1f8e73153fe1f2d558fa37ab875af652894f34c1
ss99x2002 <ss99x200211@gmail.com>
2023-07-10 22:14:56 +0900

    [Feat/#35] basic appbar 뒤로가기 클릭리스너 설정

--- app/src/main/java/com/sopt/umbba_android/presentation/setting/ManageAccountActivity.kt
+++ app/src/main/java/com/sopt/umbba_android/presentation/setting/ManageAccountActivity.kt
@@ -2,23 +2,33 @@ package com.sopt.umbba_android.presentation.setting
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import com.sopt.umbba_android.R
 import com.sopt.umbba_android.databinding.ActivityManageAccountBinding
 import com.sopt.umbba_android.util.binding.BindingActivity
 
-class ManageAccountActivity: BindingActivity<ActivityManageAccountBinding>(R.layout.activity_manage_account) {
+class ManageAccountActivity :
+    BindingActivity<ActivityManageAccountBinding>(R.layout.activity_manage_account),
+    View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.clickListener = this
         setClickEvent()
     }
 
-    private fun setClickEvent(){
-        with(binding){
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.iv_basic_back -> finish()
+        }
+    }
+
+    private fun setClickEvent() {
+        with(binding) {
             clLogout.setOnClickListener {
                 TODO("서버 로그아웃 API 연결")
             }
             clDeleteAccount.setOnClickListener {
-                startActivity(Intent(this@ManageAccountActivity,DeleteAccountActivity::class.java))
+                startActivity(Intent(this@ManageAccountActivity, DeleteAccountActivity::class.java))
             }
         }
     }
