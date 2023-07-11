3ffca8a23d029a479e9150cfa18b6d8999f822ec
ss99x2002 <ss99x200211@gmail.com>
2023-07-12 00:55:08 +0900

    [Feat/#51] Firebase Service 추가

--- app/src/main/java/com/sopt/umbba_android/util/fcm/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/sopt/umbba_android/util/fcm/MyFirebaseMessagingService.kt
@@ -1,4 +1,81 @@
 package com.sopt.umbba_android.util.fcm
 
-class MyFirebaseMessagingService {
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.sopt.umbba_android.R
+import com.sopt.umbba_android.presentation.qna.QuestionAnswerActivity
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        Log.e("hyeon", message.notification.toString())
+        Log.e("hyeon", message.data.toString())
+        createNotification(message)
+    }
+
+    private fun createNotification(message: RemoteMessage) {
+        val intent = Intent(this, QuestionAnswerActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        }
+        for (key in message.data.keys) {
+            intent.putExtra(key, message.data[key])
+        }
+
+        val pendingIntent = PendingIntent.getActivity(
+            this, (System.currentTimeMillis() / 7).toInt(), intent,
+            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_MUTABLE
+        )
+
+        val notificationChannel = NotificationChannel(
+            CHANNEL_ID,
+            "TestChannelName",
+            NotificationManager.IMPORTANCE_DEFAULT // IMPORTANCE_HIGH 면 헤드업 알림이 가능.
+        )
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_umbba_logo)
+            .setContentTitle("엄빠도 어렸다.")
+            .setContentText("알림 내용")
+            .setContentIntent(pendingIntent)
+
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(notificationChannel)
+
+        notificationManager.notify(
+            1, // 해당 알림의 고유 ID
+            builder // 표시할 알림
+                .build()
+        )
+    }
+
+    private fun getFcmToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.e("hyeon", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+            val token = task.result
+            Log.e("hyeon", "token is $token")
+        })
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "channel_id"
+        private const val notificationId = 1234
+    }
 }
