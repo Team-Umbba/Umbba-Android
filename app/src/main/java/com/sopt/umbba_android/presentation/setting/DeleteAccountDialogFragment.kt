39289b9e31d8beca47cb37253bf2739c4fa7324e
ss99x2002 <ss99x200211@gmail.com>
2023-07-11 14:08:44 +0900

    [Feat/#43] 회원 탈퇴 Dialog 연결

---
+++ app/src/main/java/com/sopt/umbba_android/presentation/setting/DeleteAccountDialogFragment.kt
@@ -0,0 +1,60 @@
+package com.sopt.umbba_android.presentation.setting
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.fragment.app.DialogFragment
+import com.sopt.umbba_android.databinding.FragemntDeleteAccountDialogBinding
+
+
+class DeleteAccountDialogFragment : DialogFragment() {
+
+    private var _binding: FragemntDeleteAccountDialogBinding? = null
+    private val binding get() = requireNotNull(_binding) { "DeleteAccountDialogFragment is null" }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragemntDeleteAccountDialogBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        backgroundDesign()
+        setBtnClickEvent()
+    }
+
+    private fun backgroundDesign() {
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    }
+
+    private fun setBtnClickEvent(){
+        with(binding){
+            btnCancel.setOnClickListener {
+                dismiss()
+            }
+            btnConfirm.setOnClickListener {
+                // TODO(탈퇴 API 연결 로직 넣기)
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        dialog?.window?.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
