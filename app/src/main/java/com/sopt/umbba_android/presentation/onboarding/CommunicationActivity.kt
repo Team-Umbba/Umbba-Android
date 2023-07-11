080fb4494b1516deafe1e3b1289da001eea50d8b
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-07-11 19:25:24 +0900

    [Design/#46] Communication Activity UI 구현

---
+++ app/src/main/java/com/sopt/umbba_android/presentation/onboarding/CommunicationActivity.kt
@@ -0,0 +1,13 @@
+package com.sopt.umbba_android.presentation.onboarding
+
+import android.os.Bundle
+import com.sopt.umbba_android.R
+import com.sopt.umbba_android.databinding.ActivityCommunicationBinding
+import com.sopt.umbba_android.util.binding.BindingActivity
+
+class CommunicationActivity : BindingActivity<ActivityCommunicationBinding>(R.layout.activity_communication) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
