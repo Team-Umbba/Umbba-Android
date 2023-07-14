5bb7fa0391168d4f31c9c85deb27a83fff9c9d28
ss99x2002 <ss99x200211@gmail.com>
2023-07-15 07:29:06 +0900

    [Feat/#61] section에 따른 배경이미지 지정

--- app/src/main/java/com/sopt/umbba_android/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/sopt/umbba_android/presentation/home/HomeFragment.kt
@@ -1,20 +1,25 @@
 package com.sopt.umbba_android.presentation.home
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import coil.load
 import com.sopt.umbba_android.R
+import com.sopt.umbba_android.data.model.response.HomeResponseDto
 import com.sopt.umbba_android.databinding.FragmentHomeBinding
 import com.sopt.umbba_android.presentation.qna.QuestionAnswerActivity
+import com.sopt.umbba_android.util.ViewModelFactory
 import com.sopt.umbba_android.util.binding.BindingFragment
 
 class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    private val viewModel: HomeViewModel by viewModels { ViewModelFactory(requireContext()) }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setBackground()
+        observeData()
         setClickEvent()
     }
 
@@ -26,9 +31,29 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home
         }
     }
 
-    private fun setBackground() {
-        with(binding) {
-            ivBackground.load(R.drawable.bg_home1)
+    private fun observeData() {
+        viewModel.homeData.observe(requireActivity()) {
+            setData(it)
+            setBackground(it.section)
         }
+    }
+
+    @SuppressLint("StringFormatMatches")
+    private fun setData(data: HomeResponseDto.HomeData) {
+        with(binding) {
+            tvTitle.text = getString(R.string.main_topic, data.index, data.topic)
+        }
+    }
+
+    private fun setBackground(section: String) {
+        binding.ivBackground.load(
+            when (section) {
+                "어린시절" -> R.drawable.bg_home1
+                "학창시절" -> R.drawable.bg_home2
+                "청춘시절" -> R.drawable.bg_home3
+                "연애시절" -> R.drawable.bg_home4
+                else -> R.drawable.bg_home5
+            }
+        )
     }
 }
