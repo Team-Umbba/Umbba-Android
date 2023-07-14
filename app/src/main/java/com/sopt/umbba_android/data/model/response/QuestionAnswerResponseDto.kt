cf68780e46ea32f1fdf4bb16e17eb6e974d9d2e5
ss99x2002 <ss99x200211@gmail.com>
2023-07-15 00:55:30 +0900

    [Feat/#57] QuestionAnswerResponseDto 추가

--- app/src/main/java/com/sopt/umbba_android/data/model/response/QuestionAnswerResponseDto.kt
+++ app/src/main/java/com/sopt/umbba_android/data/model/response/QuestionAnswerResponseDto.kt
@@ -11,30 +11,30 @@ data class QuestionAnswerResponseDto(
     val message: String,
     @SerialName("data")
     val data: QnaData
-){
+) {
     @Serializable
     data class QnaData(
-        @SerialName("is_my_answer")
-        val isMyAnswer: Boolean,
-        @SerialName("is_opponent_answer")
-        val isOpponentAnswer: Boolean,
-        @SerialName("my_answer")
-        val myAnswer: String,
-        @SerialName("my_question")
-        val myQuestion: String,
-        @SerialName("my_username")
-        val myUsername: String,
-        @SerialName("opponent_answer")
-        val opponentAnswer: String,
-        @SerialName("opponent_question")
-        val opponentQuestion: String,
-        @SerialName("opponent_username")
-        val opponentUsername: String,
         @SerialName("qna_id")
         val qnaId: Long,
         @SerialName("section")
         val section: String,
         @SerialName("topic")
-        val topic: String
+        val topic: String,
+        @SerialName("opponent_question")
+        val opponentQuestion: String,
+        @SerialName("my_question")
+        val myQuestion: String,
+        @SerialName("opponent_answer")
+        val opponentAnswer: String,
+        @SerialName("my_answer")
+        val myAnswer: String,
+        @SerialName("opponent_username")
+        val opponentUsername: String,
+        @SerialName("my_username")
+        val myUsername: String,
+        @SerialName("is_opponent_answer")
+        val isOpponentAnswer: Boolean,
+        @SerialName("is_my_answer")
+        val isMyAnswer: Boolean,
     )
 }
