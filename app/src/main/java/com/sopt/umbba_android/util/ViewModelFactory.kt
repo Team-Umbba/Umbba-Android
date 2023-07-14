07f25cee5a7982faec29a0f66ed8945cb10c222b
ss99x2002 <ss99x200211@gmail.com>
2023-07-15 08:03:06 +0900

    [Feat/#61] homeViewModel viewModelFactory 추가

--- app/src/main/java/com/sopt/umbba_android/util/ViewModelFactory.kt
+++ app/src/main/java/com/sopt/umbba_android/util/ViewModelFactory.kt
@@ -3,8 +3,11 @@ package com.sopt.umbba_android.util
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.sopt.umbba_android.data.datasource.HomeRemoteDataSource
 import com.sopt.umbba_android.data.datasource.QuestionAnswerRemoteDataSource
+import com.sopt.umbba_android.data.repository.HomeRepositoryImpl
 import com.sopt.umbba_android.data.repository.QuestionAnswerRepositoryImpl
+import com.sopt.umbba_android.presentation.home.HomeViewModel
 import com.sopt.umbba_android.presentation.qna.QuestionAnswerViewModel
 
 class ViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
@@ -13,6 +16,13 @@ class ViewModelFactory(private val context: Context) : ViewModelProvider.Factory
             return QuestionAnswerViewModel(
                 QuestionAnswerRepositoryImpl(
                     QuestionAnswerRemoteDataSource()
+                )
+            ) as T
+        }
+        if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
+            return HomeViewModel(
+                HomeRepositoryImpl(
+                    HomeRemoteDataSource()
                 )
             ) as T
         }
