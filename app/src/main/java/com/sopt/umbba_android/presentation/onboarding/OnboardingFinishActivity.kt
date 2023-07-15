68496787dd2075ec6632d102f519743dce46d404
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-07-16 03:05:29 +0900

    [Feat/#65] fade in & fade out 애니메이션 적용

--- app/src/main/java/com/sopt/umbba_android/presentation/onboarding/OnboardingFinishActivity.kt
+++ app/src/main/java/com/sopt/umbba_android/presentation/onboarding/OnboardingFinishActivity.kt
@@ -2,11 +2,32 @@ package com.sopt.umbba_android.presentation.onboarding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.animation.AnimationUtils
 import com.sopt.umbba_android.R
+import com.sopt.umbba_android.databinding.ActivityOnboardingFinishBinding
+import com.sopt.umbba_android.util.binding.BindingActivity
 
-class OnboardingFinishActivity : AppCompatActivity() {
+class OnboardingFinishActivity : BindingActivity<ActivityOnboardingFinishBinding>(R.layout.activity_onboarding_finish) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboarding_finish)
+
+        animateFadeBackground()
+    }
+
+    private fun animateFadeBackground() {
+        val fadeOutAnim = AnimationUtils.loadAnimation(this, R.anim.fadeout)
+        with(binding.clStart) {
+            startAnimation(fadeOutAnim)
+            visibility = View.INVISIBLE
+        }
+        val fadeInAnim = AnimationUtils.loadAnimation(this, R.anim.fadein)
+        binding.clArrive.startAnimation(fadeInAnim)
+        with(binding.clArrive) {
+            startAnimation(fadeInAnim)
+            visibility = View.VISIBLE
+        }
     }
 }
