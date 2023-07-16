b20004e848fef332d203b4ad5d5befaa464a5a3b
ss99x2002 <ss99x200211@gmail.com>
2023-07-17 01:35:43 +0900

    [Chore/#68] list 이미지 추가

--- app/src/main/java/com/sopt/umbba_android/presentation/list/ListFragment.kt
+++ app/src/main/java/com/sopt/umbba_android/presentation/list/ListFragment.kt
@@ -2,11 +2,7 @@ package com.sopt.umbba_android.presentation.list
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import coil.load
 import com.sopt.umbba_android.R
