82e46e4b33ddbdd2a1cfb8f0eaa201b5b608b5f0
ss99x2002 <ss99x200211@gmail.com>
2023-07-16 18:41:15 +0900

    [Feat/#68] ListAdapter 연결

--- app/src/main/java/com/sopt/umbba_android/presentation/list/ListQuestionAdapter.kt
+++ app/src/main/java/com/sopt/umbba_android/presentation/list/ListQuestionAdapter.kt
@@ -6,15 +6,16 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.sopt.umbba_android.data.model.response.ExampleResponseDto
+import com.sopt.umbba_android.data.model.response.ListResponseDto
 import com.sopt.umbba_android.databinding.ItemQuestionListBinding
 
-class ListQuestionAdapter(private val itemClick: (ExampleResponseDto) -> (Unit)) :
-    ListAdapter<ExampleResponseDto, ListQuestionAdapter.ListViewHolder>(diffUtil) {
+class ListQuestionAdapter(private val itemClick: (ListResponseDto.ListData) -> (Unit)) :
+    ListAdapter<ListResponseDto.ListData, ListQuestionAdapter.ListViewHolder>(diffUtil) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
         val binding =
             ItemQuestionListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ListViewHolder(binding)
+        return ListViewHolder(binding,itemClick)
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
@@ -22,25 +23,32 @@ class ListQuestionAdapter(private val itemClick: (ExampleResponseDto) -> (Unit))
     }
 
     class ListViewHolder(
-        private val binding: ItemQuestionListBinding
+        private val binding: ItemQuestionListBinding,
+        private val itemClick: (ListResponseDto.ListData) -> (Unit)
     ) : RecyclerView.ViewHolder(binding.root) {
-        fun onBind(data: ExampleResponseDto) {
-            TODO("서버 연결할 때 채우기")
+        fun onBind(data: ListResponseDto.ListData) {
+            with(binding) {
+                tvIndex.text = data.index.toString()
+                tvTopic.text = data.topic
+                root.setOnClickListener {
+                    itemClick(data)
+                }
+            }
         }
     }
 
     companion object {
-        val diffUtil = object : DiffUtil.ItemCallback<ExampleResponseDto>() {
+        val diffUtil = object : DiffUtil.ItemCallback<ListResponseDto.ListData>() {
             override fun areItemsTheSame(
-                oldItem: ExampleResponseDto,
-                newItem: ExampleResponseDto
+                oldItem: ListResponseDto.ListData,
+                newItem: ListResponseDto.ListData
             ): Boolean {
-                return oldItem.id == newItem.id
+                return oldItem.qnaId == newItem.qnaId
             }
 
             override fun areContentsTheSame(
-                oldItem: ExampleResponseDto,
-                newItem: ExampleResponseDto
+                oldItem: ListResponseDto.ListData,
+                newItem: ListResponseDto.ListData
             ): Boolean {
                 return oldItem == newItem
             }
