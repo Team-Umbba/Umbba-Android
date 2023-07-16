68fd1e59b2a404ec15a66876287c45dbb904e186
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-07-17 02:20:13 +0900

    [Feat/#66] 버튼 초기화 후 활성화 로직 추가

--- app/src/main/java/com/sopt/umbba_android/presentation/onboarding/quest/QuestThreeFragment.kt
+++ app/src/main/java/com/sopt/umbba_android/presentation/onboarding/quest/QuestThreeFragment.kt
@@ -2,13 +2,33 @@ package com.sopt.umbba_android.presentation.onboarding.quest
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import com.sopt.umbba_android.R
 import com.sopt.umbba_android.databinding.FragmentQuestThreeBinding
 import com.sopt.umbba_android.util.binding.BindingFragment
 
 class QuestThreeFragment : BindingFragment<FragmentQuestThreeBinding>(R.layout.fragment_quest_three) {
 
+    private val viewModel by activityViewModels<QuestViewModel>()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.viewmodel = viewModel
+
+        checkButtonComplete()
+    }
+
+    private fun checkButtonComplete() {
+        viewModel.isClickedYes.observe(viewLifecycleOwner) {
+            viewModel.checkButtonComplete()
+            viewModel.clickedChipText.value = binding.btnAnswer1.text.toString()
+        }
+        viewModel.isClickedNo.observe(viewLifecycleOwner) {
+            viewModel.checkButtonComplete()
+            viewModel.clickedChipText.value = binding.btnAnswer2.text.toString()
+        }
+        viewModel.isClickedAmbiguous.observe(viewLifecycleOwner) {
+            viewModel.checkButtonComplete()
+            viewModel.clickedChipText.value = binding.btnAnswer3.text.toString()
+        }
     }
 }
