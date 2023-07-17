8b778e3b819c8418c92e3e0c6740249ad2729fe8
ss99x2002 <ss99x200211@gmail.com>
2023-07-18 01:26:04 +0900

    [Feat/#73] setting 서비스 추가

--- app/src/main/java/com/sopt/umbba_android/data/repository/SettingRepositoryImpl.kt
+++ app/src/main/java/com/sopt/umbba_android/data/repository/SettingRepositoryImpl.kt
@@ -1,13 +1,20 @@
 package com.sopt.umbba_android.data.repository
 
 import com.sopt.umbba_android.data.datasource.SettingRemoteDataSource
+import com.sopt.umbba_android.data.model.response.LogOutResponseDto
 import com.sopt.umbba_android.domain.repository.SettingRepository
+import timber.log.Timber
 
 class SettingRepositoryImpl(private val settingRemoteDataSource: SettingRemoteDataSource) :
     SettingRepository {
-    override suspend fun logout() {
-        TODO("Not yet implemented")
-    }
+    override suspend fun logout():Result<LogOutResponseDto> =
+        runCatching {
+            settingRemoteDataSource.logout()
+        }.onSuccess {
+            Timber.e("로그아웃 성공")
+        }.onFailure {
+            Timber.e("로그아웃 실패")
+        }
 
     override suspend fun deleteAccount() {
         TODO("Not yet implemented")
--- app/src/main/java/com/sopt/umbba_android/domain/repository/SettingRepository.kt
+++ app/src/main/java/com/sopt/umbba_android/domain/repository/SettingRepository.kt
@@ -1,6 +1,8 @@
 package com.sopt.umbba_android.domain.repository
 
+import com.sopt.umbba_android.data.model.response.LogOutResponseDto
+
 interface SettingRepository {
-    suspend fun logout()
+    suspend fun logout():Result<LogOutResponseDto>
     suspend fun deleteAccount()
 }
