e7267d71214d6e1aae3e19d940796170a48cbbfd
ss99x2002 <ss99x200211@gmail.com>
2023-07-18 01:26:27 +0900

    [Feat/#73] viewModel Factory 추가

--- app/src/main/java/com/sopt/umbba_android/util/ViewModelFactory.kt
+++ app/src/main/java/com/sopt/umbba_android/util/ViewModelFactory.kt
@@ -6,14 +6,17 @@ import androidx.lifecycle.ViewModelProvider
 import com.sopt.umbba_android.data.datasource.HomeRemoteDataSource
 import com.sopt.umbba_android.data.datasource.ListRemoteDataSource
 import com.sopt.umbba_android.data.datasource.QuestionAnswerRemoteDataSource
+import com.sopt.umbba_android.data.datasource.SettingRemoteDataSource
 import com.sopt.umbba_android.data.repository.HomeRepositoryImpl
 import com.sopt.umbba_android.data.repository.ListRepositoryImpl
 import com.sopt.umbba_android.data.repository.QuestionAnswerRepositoryImpl
+import com.sopt.umbba_android.data.repository.SettingRepositoryImpl
 import com.sopt.umbba_android.presentation.home.viewmodel.HomeViewModel
 import com.sopt.umbba_android.presentation.list.viewmodel.ListViewModel
 import com.sopt.umbba_android.presentation.qna.viewmodel.AnswerViewModel
 import com.sopt.umbba_android.presentation.qna.viewmodel.ConfirmAnswerDialogFragmentViewModel
 import com.sopt.umbba_android.presentation.qna.viewmodel.QuestionAnswerViewModel
+import com.sopt.umbba_android.presentation.setting.viewmodel.ManageAccountViewModel
 
 class ViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
@@ -47,6 +50,13 @@ class ViewModelFactory(private val context: Context) : ViewModelProvider.Factory
             return ListViewModel(
                 ListRepositoryImpl(
                     ListRemoteDataSource()
+                )
+            ) as T
+        }
+        if (modelClass.isAssignableFrom(ManageAccountViewModel::class.java)) {
+            return ManageAccountViewModel(
+                SettingRepositoryImpl(
+                    SettingRemoteDataSource()
                 )
             ) as T
         }
