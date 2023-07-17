5e1d1a8640c3011c8d15f0078a59fff8a2becfc1
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-07-17 18:14:55 +0900

    [Setting/#72] 카카오 로그인 기초 세팅

--- app/src/main/java/com/sopt/umbba_android/application/UmbbaApplication.kt
+++ app/src/main/java/com/sopt/umbba_android/application/UmbbaApplication.kt
@@ -2,10 +2,13 @@ package com.sopt.umbba_android.application
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import com.kakao.sdk.common.KakaoSdk
+import com.sopt.umbba_android.BuildConfig.KAKAO_NATIVE_APP_KEY
 
 class UmbbaApplication: Application() {
     override fun onCreate() {
         super.onCreate()
+        KakaoSdk.init(this, KAKAO_NATIVE_APP_KEY)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
