7d6d272f146d53ea43053e7e7389285402fa4ee9
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-07-19 00:23:40 +0900

    [Feat/#79] Api Factory에 서비스 추가

--- app/src/main/java/com/sopt/umbba_android/data/model/ApiFactory.kt
+++ app/src/main/java/com/sopt/umbba_android/data/model/ApiFactory.kt
@@ -4,7 +4,7 @@ import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFact
 import com.sopt.umbba_android.BuildConfig
 import com.sopt.umbba_android.data.service.HomeService
 import com.sopt.umbba_android.data.service.ListService
-import com.sopt.umbba_android.data.service.OnBoardingService
+import com.sopt.umbba_android.data.service.OnboardingService
 import com.sopt.umbba_android.data.service.QuestionAnswerService
 import com.sopt.umbba_android.data.service.SettingService
 import com.sopt.umbba_android.data.interceptor.AuthInterceptor
@@ -12,7 +12,6 @@ import com.sopt.umbba_android.data.service.LoginService
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 
 object ApiFactory {
@@ -39,7 +38,7 @@ object ServicePool {
     val questionAnswerService = ApiFactory.create<QuestionAnswerService>()
     val settingService = ApiFactory.create<SettingService>()
     val homeService = ApiFactory.create<HomeService>()
-    val onBoardingService = ApiFactory.create<OnBoardingService>()
+    val onboardingService = ApiFactory.create<OnboardingService>()
     val listService = ApiFactory.create<ListService>()
     val loginService = ApiFactory.create<LoginService>()
 }
