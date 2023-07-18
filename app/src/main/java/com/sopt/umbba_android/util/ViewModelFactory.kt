c226e7b4adcde1cebb4d2ef957cb889c974c612f
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-07-19 00:44:57 +0900

    [Feat/#79] ViewModelFactory에 InviteCodeViewModel 추가

--- app/src/main/java/com/sopt/umbba_android/util/ViewModelFactory.kt
+++ app/src/main/java/com/sopt/umbba_android/util/ViewModelFactory.kt
@@ -6,14 +6,18 @@ import androidx.lifecycle.ViewModelProvider
 import com.sopt.umbba_android.data.datasource.HomeRemoteDataSource
 import com.sopt.umbba_android.data.datasource.ListRemoteDataSource
 import com.sopt.umbba_android.data.datasource.LoginRemoteDataSource
+import com.sopt.umbba_android.data.datasource.OnboardingRemoteDataSource
 import com.sopt.umbba_android.data.datasource.QuestionAnswerRemoteDataSource
 import com.sopt.umbba_android.data.model.ServicePool
 import com.sopt.umbba_android.data.repository.HomeRepositoryImpl
 import com.sopt.umbba_android.data.repository.ListRepositoryImpl
 import com.sopt.umbba_android.data.repository.LoginRepositoryImpl
+import com.sopt.umbba_android.data.repository.OnboardingRepositoryImpl
 import com.sopt.umbba_android.data.repository.QuestionAnswerRepositoryImpl
 import com.sopt.umbba_android.data.service.LoginService
+import com.sopt.umbba_android.domain.repository.OnboardingRepository
 import com.sopt.umbba_android.presentation.home.viewmodel.HomeViewModel
+import com.sopt.umbba_android.presentation.invite.viewmodel.InviteCodeViewModel
 import com.sopt.umbba_android.presentation.list.viewmodel.ListViewModel
 import com.sopt.umbba_android.presentation.login.viewmodel.LoginViewModel
 import com.sopt.umbba_android.presentation.qna.viewmodel.AnswerViewModel
@@ -60,6 +64,14 @@ class ViewModelFactory(private val context: Context) : ViewModelProvider.Factory
             return LoginViewModel(
                 LoginRepositoryImpl(
                     LoginRemoteDataSource()
+                )
+            ) as T
+        }
+
+        if (modelClass.isAssignableFrom(InviteCodeViewModel::class.java)) {
+            return InviteCodeViewModel(
+                OnboardingRepositoryImpl(
+                    OnboardingRemoteDataSource()
                 )
             ) as T
         }
