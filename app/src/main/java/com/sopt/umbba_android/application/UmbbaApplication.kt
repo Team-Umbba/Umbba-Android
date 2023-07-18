bae2ac65026236f18af50958de0ca5702a58b4e4
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-07-18 18:11:49 +0900

    [Feat/#72] 애플리케이션에 자동로그인 세팅

--- app/src/main/java/com/sopt/umbba_android/application/UmbbaApplication.kt
+++ app/src/main/java/com/sopt/umbba_android/application/UmbbaApplication.kt
@@ -4,11 +4,21 @@ import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
 import com.kakao.sdk.common.KakaoSdk
 import com.sopt.umbba_android.BuildConfig.KAKAO_NATIVE_APP_KEY
+import com.sopt.umbba_android.data.local.SharedPreferences
 
 class UmbbaApplication: Application() {
     override fun onCreate() {
         super.onCreate()
-        KakaoSdk.init(this, KAKAO_NATIVE_APP_KEY)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        initKakao()
+        setupSharedPreferences()
+    }
+
+    private fun initKakao() {
+        KakaoSdk.init(this, KAKAO_NATIVE_APP_KEY)
+    }
+
+    private fun setupSharedPreferences() {
+        SharedPreferences.init(this)
     }
 }
