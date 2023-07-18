8e666b310970619d39488f0a85094f6f4b1d3e4c
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-07-18 18:04:43 +0900

    [Setting/#72] 자동 로그인 세팅

---
+++ app/src/main/java/com/sopt/umbba_android/data/local/SharedPreferences.kt
@@ -0,0 +1,65 @@
+package com.sopt.umbba_android.data.local
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
+import com.sopt.umbba_android.BuildConfig
+import com.sopt.umbba_android.presentation.login.LoginActivity
+
+const val FILE_NAME = "UMBBA"
+
+object SharedPreferences {
+    private lateinit var preferences: SharedPreferences
+
+    fun init(context: Context) {
+        val masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+        preferences =
+            if (BuildConfig.DEBUG) {
+                context.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE)
+            } else {
+                EncryptedSharedPreferences.create(
+                    FILE_NAME,
+                    masterKeyAlias,
+                    context,
+                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+                )
+            }
+    }
+
+    fun setKakaoString(key: String, value: String?) {
+        preferences.edit { putString(key, value) }
+    }
+
+    fun getKakaoString(key: String): String? {
+        return preferences.getString(key, null)
+    }
+
+    //토큰 저장
+    fun setString(key: String, value: String?) {
+        preferences.edit { putString(key, value) }
+    }
+    //토큰 가져오기 - 자동 로그인 확인
+    fun getString(key: String): String? {
+        return preferences.getString(key, null)
+    }
+    //온보딩 완료 상태 저장
+    fun setBoolean(key: String, value: Boolean) {
+        preferences.edit { putBoolean(key, value) }
+    }
+    //온보딩 완료했는지 가져오기 - 온보딩 완료 확인
+    fun getBoolean(key: String): Boolean {
+        return preferences.getBoolean(key, false)
+    }
+
+    private fun clear() {
+        preferences.edit { clear() }
+    }
+
+    fun clearForLogout() {
+        clear()
+        setBoolean(LoginActivity.DID_USER_CLEAR_ONBOARD, true)
+    }
+}
