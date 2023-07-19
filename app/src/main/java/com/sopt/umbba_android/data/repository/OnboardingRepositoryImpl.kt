297e58a8b2a3ffc02fa7357ae05df02858976331
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-07-20 05:12:37 +0900

    [Feat/#88] DataSource, Repository, 구현체 생성

--- app/src/main/java/com/sopt/umbba_android/data/repository/OnboardingRepositoryImpl.kt
+++ app/src/main/java/com/sopt/umbba_android/data/repository/OnboardingRepositoryImpl.kt
@@ -3,7 +3,11 @@ package com.sopt.umbba_android.data.repository
 import android.util.Log
 import com.sopt.umbba_android.data.datasource.OnboardingRemoteDataSource
 import com.sopt.umbba_android.data.model.request.InviteCodeRequestDto
+import com.sopt.umbba_android.data.model.request.ReceiveInfoRequestDto
+import com.sopt.umbba_android.data.model.request.SendInfoRequestDto
 import com.sopt.umbba_android.data.model.response.InviteCodeResponseDto
+import com.sopt.umbba_android.data.model.response.ReceiveInfoResponseDto
+import com.sopt.umbba_android.data.model.response.SendInfoResponseDto
 import com.sopt.umbba_android.domain.repository.OnboardingRepository
 
 class OnboardingRepositoryImpl(
@@ -13,8 +17,26 @@ class OnboardingRepositoryImpl(
         runCatching {
             onboardingRemoteDataSource.setFamily(inviteCodeRequestDto)
         }.onSuccess {
-            Log.e("yeonjin", "onboarding Impl 성공")
+            Log.e("yeonjin", "onboarding setFamily Impl 성공")
         }.onFailure {
-            Log.e("yeonjin", "onboarding Impl 실패")
+            Log.e("yeonjin", "onboarding setFamily Impl 실패")
+        }
+
+    override suspend fun setSendInfo(sendInfoRequestDto: SendInfoRequestDto): Result<SendInfoResponseDto> =
+        runCatching {
+            onboardingRemoteDataSource.setSendInfo(sendInfoRequestDto)
+        }.onSuccess {
+            Log.e("yeonjin", "onboarding setSendInfo Impl 성공")
+        }.onFailure {
+            Log.e("yeonjin", "onboarding setSendInfo Impl 성공")
+        }
+
+    override suspend fun setReceiveInfo(receiveInfoRequestDto: ReceiveInfoRequestDto): Result<ReceiveInfoResponseDto> =
+        runCatching {
+            onboardingRemoteDataSource.setReceiveInfo(receiveInfoRequestDto)
+        }.onSuccess {
+            Log.e("yeonjin", "onboarding setReceiveInfo Impl 성공")
+        }.onFailure {
+            Log.e("yeonjin", "onboarding setReceiveInfo Impl 성공")
         }
 }
