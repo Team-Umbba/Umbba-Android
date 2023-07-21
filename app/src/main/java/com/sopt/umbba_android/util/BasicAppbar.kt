eb7a993b7bc5aad4cac1d906b62a2b280f0e3dae
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-07-21 17:48:42 +0900

    [Chore/#98] 이름 및 폴더링 변경

--- app/src/main/java/com/sopt/umbba_android/util/BasicAppbar.kt
+++ app/src/main/java/com/sopt/umbba_android/util/BasicAppbar.kt
@@ -1,15 +1,12 @@
-package com.sopt.umbba_android.presentation
+package com.sopt.umbba_android.util
 
-import android.content.Context
 import android.os.Bundle
-import android.util.AttributeSet
 import android.view.View
-import android.view.View.OnClickListener
 import com.sopt.umbba_android.R
 import com.sopt.umbba_android.databinding.BasicAppbarBinding
 import com.sopt.umbba_android.util.binding.BindingFragment
 
-class BasicAppbarFragment : BindingFragment<BasicAppbarBinding>(R.layout.basic_appbar) {
+class BasicAppbar : BindingFragment<BasicAppbarBinding>(R.layout.basic_appbar) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
