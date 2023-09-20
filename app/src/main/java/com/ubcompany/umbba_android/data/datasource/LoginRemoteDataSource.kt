6b7fb99a9dd1cb1b2bf64cd5c05770ee027b5c91
ss99x2002 <ss99x200211@gmail.com>
2023-09-20 18:02:31 +0900

    [Refactor/#126] DataSource 생성자 주입 추가

--- app/src/main/java/com/ubcompany/umbba_android/data/datasource/LoginRemoteDataSource.kt
+++ app/src/main/java/com/ubcompany/umbba_android/data/datasource/LoginRemoteDataSource.kt
@@ -3,9 +3,12 @@ package com.ubcompany.umbba_android.data.datasource
 import com.ubcompany.umbba_android.data.model.ServicePool
 import com.ubcompany.umbba_android.data.model.request.LoginRequestDto
 import com.ubcompany.umbba_android.data.model.response.LoginResponseDto
+import com.ubcompany.umbba_android.data.service.LoginService
+import javax.inject.Inject
 
-class LoginRemoteDataSource {
-    private val loginService = ServicePool.loginService
+class LoginRemoteDataSource  @Inject constructor(
+    private val loginService : LoginService
+) {
     suspend fun postLogin(loginRequestDto: LoginRequestDto): LoginResponseDto =
         loginService.postLogin(loginRequestDto)
 }
--- app/src/main/java/com/ubcompany/umbba_android/data/datasource/OnboardingRemoteDataSource.kt
+++ app/src/main/java/com/ubcompany/umbba_android/data/datasource/OnboardingRemoteDataSource.kt
@@ -1,15 +1,17 @@
 package com.ubcompany.umbba_android.data.datasource
 
-import com.ubcompany.umbba_android.data.model.ServicePool
 import com.ubcompany.umbba_android.data.model.request.InviteCodeRequestDto
 import com.ubcompany.umbba_android.data.model.request.ReceiveInfoRequestDto
 import com.ubcompany.umbba_android.data.model.request.SendInfoRequestDto
 import com.ubcompany.umbba_android.data.model.response.InviteCodeResponseDto
 import com.ubcompany.umbba_android.data.model.response.ReceiveInfoResponseDto
 import com.ubcompany.umbba_android.data.model.response.SendInfoResponseDto
+import com.ubcompany.umbba_android.data.service.OnboardingService
+import javax.inject.Inject
 
-class OnboardingRemoteDataSource {
-    private val onboardingService = ServicePool.onboardingService
+class OnboardingRemoteDataSource @Inject constructor(
+    private val onboardingService: OnboardingService
+) {
     suspend fun setFamily(onboardingRequestDto: InviteCodeRequestDto): InviteCodeResponseDto =
         onboardingService.setFamily(onboardingRequestDto)
 
