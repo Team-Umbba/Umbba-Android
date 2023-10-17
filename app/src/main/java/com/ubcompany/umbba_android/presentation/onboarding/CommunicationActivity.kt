7bcd086a03ff2c72f6429f31928892887d74e5e8
Hwang Yeonjin <yjin1100@sookmyung.ac.kr>
2023-10-17 21:38:59 +0900

    [Design/#136] 교신화면 고도화 (#139)
    * [Design/#136] 교신화면 고도화
    
    * [chore/#136] 이미지 변경
    
    * [chore/#136] 이미지 수정
    
    * [Refactor/#136] 애니메이션 시간 수정

--- app/src/main/java/com/ubcompany/umbba_android/presentation/onboarding/CommunicationActivity.kt
+++ app/src/main/java/com/ubcompany/umbba_android/presentation/onboarding/CommunicationActivity.kt
@@ -3,6 +3,14 @@ package com.ubcompany.umbba_android.presentation.onboarding
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.animation.AnimationUtils
+import coil.ImageLoader
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
+import coil.load
 import com.ubcompany.umbba_android.R
 import com.ubcompany.umbba_android.databinding.ActivityCommunicationBinding
 import com.ubcompany.umbba_android.domain.entity.User
@@ -17,6 +25,8 @@ class CommunicationActivity :
         super.onCreate(savedInstanceState)
 
         goInputInfoActivity()
+        initLoadingGif()
+        animateFadeBackground()
     }
 
     private fun goInputInfoActivity() {
@@ -31,4 +41,33 @@ class CommunicationActivity :
             })
         }
     }
+
+    private fun initLoadingGif() {
+        val imageLoader = ImageLoader.Builder(applicationContext)
+            .components {
+                if (Build.VERSION.SDK_INT >= 28) {
+                    add(ImageDecoderDecoder.Factory())
+                } else {
+                    add(GifDecoder.Factory())
+                }
+            }.build()
+
+        binding.ivGif.load(R.raw.communication, imageLoader = imageLoader)
+    }
+
+    private fun animateFadeBackground() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val fadeOutAnim = AnimationUtils.loadAnimation(this, R.anim.fadeout_short)
+            with(binding.clStart) {
+                startAnimation(fadeOutAnim)
+                visibility = View.INVISIBLE
+            }
+        }, 2000)
+        Handler(Looper.getMainLooper()).postDelayed({
+            with(binding.clEnd) {
+                visibility = View.VISIBLE
+            }
+        }, 3000)
+    }
+
 }
