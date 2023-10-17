1ecd6c45f1952cd05d44098e0777de211d08e36b
ss99x2002 <ss99x200211@gmail.com>
2023-10-17 20:56:45 +0900

    [Design/#137] 답변 확인 dialog 문구 수정

--- app/src/main/java/com/ubcompany/umbba_android/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/ubcompany/umbba_android/presentation/home/HomeFragment.kt
@@ -10,14 +10,6 @@ import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
 import coil.load
-import com.google.android.play.core.appupdate.AppUpdateManager
-import com.google.android.play.core.appupdate.AppUpdateManagerFactory
-import com.google.android.play.core.install.model.AppUpdateType
-import com.google.android.play.core.install.model.UpdateAvailability
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.remoteconfig.ktx.remoteConfig
-import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
-import com.ubcompany.umbba_android.BuildConfig
 import com.ubcompany.umbba_android.R
 import com.ubcompany.umbba_android.data.local.SharedPreferences
 import com.ubcompany.umbba_android.data.model.response.HomeCaseResponseDto
