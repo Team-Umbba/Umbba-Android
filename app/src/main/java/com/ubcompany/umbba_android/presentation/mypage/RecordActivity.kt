9979a26d30cc1be03a7ef9d1cd09c3faaa4c4616
yeoncheong <yjin1100@sookmyung.ac.kr>
2024-03-24 20:15:05 +0900

    [Chore/#163] 상단바 교체

--- app/src/main/java/com/ubcompany/umbba_android/presentation/mypage/RecordActivity.kt
+++ app/src/main/java/com/ubcompany/umbba_android/presentation/mypage/RecordActivity.kt
@@ -2,6 +2,7 @@ package com.ubcompany.umbba_android.presentation.mypage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.ubcompany.umbba_android.R
 import com.ubcompany.umbba_android.databinding.ActivityRecordBinding
 import com.ubcompany.umbba_android.util.binding.BindingActivity
@@ -9,16 +10,17 @@ import com.ubcompany.umbba_android.util.setOnSingleClickListener
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class RecordActivity : BindingActivity<ActivityRecordBinding>(R.layout.activity_record) {
+class RecordActivity : BindingActivity<ActivityRecordBinding>(R.layout.activity_record),
+    View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        goBack()
+        binding.clickListener = this
+        
     }
 
-    private fun goBack() {
-        binding.btnBack.setOnSingleClickListener {
-            finish()
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.iv_basic_back -> finish()
         }
     }
 
