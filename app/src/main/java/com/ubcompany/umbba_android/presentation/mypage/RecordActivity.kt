a6ed5d138b6fe68f9a6eb896249f9e1ac5b66c0b
yeoncheong <yjin1100@sookmyung.ac.kr>
2024-04-30 20:50:59 +0900

    [Feat/#169] 갤러리 접근 권한 허용 기능 추가

--- app/src/main/java/com/ubcompany/umbba_android/presentation/mypage/RecordActivity.kt
+++ app/src/main/java/com/ubcompany/umbba_android/presentation/mypage/RecordActivity.kt
@@ -19,6 +19,17 @@ class RecordActivity : BindingActivity<ActivityRecordBinding>(R.layout.activity_
     View.OnClickListener {
 
     private val viewModel by viewModels<RecordViewModel>()
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                Snackbar.make(binding.root, "갤러리 접근 권한이 허용되어 있습니다.", Snackbar.LENGTH_SHORT)
+                    .show()
+            } else {
+                Snackbar.make(binding.root, "갤러리에 접근할 권한이 없습니다.", Snackbar.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
     private val launcher =
         registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { imageUrl: Uri? ->
             // 서버에 put으로 파일 넘기기
@@ -50,5 +61,45 @@ class RecordActivity : BindingActivity<ActivityRecordBinding>(R.layout.activity_
                 }
             }
         }
+    private fun askNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_MEDIA_IMAGES
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                Snackbar.make(
+                    binding.root,
+                    "갤러리 접근 권한이 허용되어 있습니다.",
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            } else {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.READ_MEDIA_IMAGES)) {
+                    showPermissionContextPopup()
+                } else {
+                    requestPermissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                }
+            }
+        }
     }
+
+    private fun showPermissionContextPopup() {
+        AlertDialog.Builder(this)
+            .setTitle("권한이 필요합니다.")
+            .setMessage("앱에서 사진을 불러오기 위해 권한이 필요합니다.")
+            .setPositiveButton("동의") { _, _ ->
+                requestPermissions(
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    PERMISSION_ALBUM
+                )
+            }
+            .setNegativeButton("취소") { _, _ -> }
+            .create()
+            .show()
+    }
+
+    companion object {
+        private val PERMISSION_ALBUM = 101
+    }
+
 }
