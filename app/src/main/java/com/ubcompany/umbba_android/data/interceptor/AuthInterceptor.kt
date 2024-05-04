0291d1adc9aa0a7740b0db99482fb8e97f9b6a70
yeoncheong <yjin1100@sookmyung.ac.kr>
2024-05-05 00:16:48 +0900

    [Fix/#182] URL 검사하여 base url이 아닐 경우 헤더 제거하는 코드 추가

--- app/src/main/java/com/ubcompany/umbba_android/data/interceptor/AuthInterceptor.kt
+++ app/src/main/java/com/ubcompany/umbba_android/data/interceptor/AuthInterceptor.kt
@@ -1,9 +1,11 @@
 package com.ubcompany.umbba_android.data.interceptor
 
+import com.ubcompany.umbba_android.BuildConfig
 import com.ubcompany.umbba_android.data.local.SharedPreferences
 import com.ubcompany.umbba_android.presentation.login.LoginActivity.Companion.USER_KAKAO_TOKEN
 import com.ubcompany.umbba_android.presentation.login.LoginActivity.Companion.USER_TOKEN
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
 import javax.inject.Inject
 
@@ -19,12 +21,15 @@ class AuthInterceptor @Inject constructor() : Interceptor {
 
         val originalRequest = chain.request()
 
-        val headerRequest = originalRequest.newBuilder()
-            .header(
-                "Authorization",
-                "$token"
-            )
-            .build()
+        val headerRequest = originalRequest.newAuthBuilder("$token")
         return chain.proceed(headerRequest)
     }
+
+    private fun Request.newAuthBuilder(token: String): Request =
+        if (BuildConfig.UMBBA_BASE_URL.contains(url.host)) {
+            this.newBuilder()
+                .addHeader("Authorization", token)
+                .build()
+        }
+        else this
 }
